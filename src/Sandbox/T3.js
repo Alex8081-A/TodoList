256028657b1796ed684ba56a3f5ca6161761fc36
Alex8081-A <vladaleksiyevich@gmail.com>
2023-11-22 12:38:03 +0300

    T3

---
+++ src/Sandbox/T3.js
@@ -0,0 +1,17 @@
+import Select from "./Select";
+import Text from "./Text";
+import Checkbox from "./Checkbox";
+
+
+
+function App() {
+    return (
+        <div>
+            <Text />
+            <Select/>
+            <Checkbox/>
+        </div>
+    );
+}
+
+export default App;
