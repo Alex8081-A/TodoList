60893bda0bf06497c7c519e17281a0d80993f36f
Alex8081-A <vladaleksiyevich@gmail.com>
2023-12-21 12:42:18 +0300

    1. Уведомление при авторизации пользователя 2. Токен в local storage при нажатии чекбокса 3. Компонент-обёртка пускает на другие страницы только при наличии токена 4. Создал слайс для юзера

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from "react";
 import Login from "./Pages/Login";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-
+import Wrapper from "./Sandbox/Wrapper";
 const App = () => {
+  const checkToken = () => {
+    const token = localStorage.getItem("token");
+    if (token === "secret-string") {
+      return true;
+    } else return false;
+  };
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="Home" element={<Home />} />
+        <Route
+          path="Home"
+          element={
+            <Wrapper isAuth={checkToken()}>
+              <Home />
+            </Wrapper>
+          }
+        />
+        <Route
+          path="About"
+          element={
+            <Wrapper isAuth={checkToken()}>
+              <div>adas</div>
+            </Wrapper>
+          }
+        />
       </Routes>
     </div>
   );
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,36 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../components/Form/Form";
 import { useNavigate } from "react-router-dom";
+
 const Login = () => {
   let navigate = useNavigate();
   const goHome = () => {
     navigate("Home");
   };
+  const [auth, setAuth] = useState(false);
+  const change = () => {
+    setAuth(true);
+  };
   async function handleAuth(data) {
     let promise = new Promise((resolve, reject) => {
       if (data.password === "admin" && data.login === "admin") {
         setTimeout(() => resolve({ token: "secret-string" }), 2000);
+        change();
       } else {
         setTimeout(() => reject("Ошибка"), 2000);
+      }
+      if (data.checkbox === true) {
+        localStorage.setItem("token", "secret-string");
       }
     });
     try {
       let result = await promise;
       console.log(result);
-      /* if ((data.checkbox = true)) {
-        localStorage.setItem("name", "value");
-      }*/
-      // console.log(data.checkbox);
-      // localStorage.setItem();
       goHome();
     } catch (error) {
       console.log(error);
     }
-    /*console.log(promise);*/
+    // console.log(promise);
   }
   return (
     <div>
       <h1>Авторизация</h1>
+      {auth ? <p>Успешная авторизация</p> : undefined}
       <Form
         onSubmit={handleAuth}
         config={[
