1df13e6c3e0ab23a5e1c8722db6d715a0c13cbb5
Alex8081-A <vladaleksiyevich@gmail.com>
2024-01-17 22:32:34 +0300

    Стилизация компонентов, промис, токен, обёртка

---
+++ src/utils/userAuth.js
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { checkToken } from "./utils";
+import { useDispatch } from "react-redux";
+import { setUser } from "../store/userSlice";
+export const useAuth = () => {
+  const { isAuth } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  if (checkToken()) {
+    dispatch(
+      setUser({
+        isAuth: true,
+      })
+    );
+  } else {
+    dispatch(
+      setUser({
+        isAuth: false,
+      })
+    );
+  }
+  return { isAuth };
+};
