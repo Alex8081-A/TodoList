92e95d11657a183bceae9e34646839488a2d27f1
Alex8081-A <vladaleksiyevich@gmail.com>
2024-01-17 22:13:46 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import T4 from "./Sandbox/T4";
-import React from "react";
-//import Test from './Sandbox/Test';
+import React, { useState } from "react";
+import Login from "./Pages/Login";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Wrapper from "./Sandbox/Wrapper";
+import { useAuth } from "./utils/userAuth";
+import Spinner from "./Spinner";
 
 const App = () => {
+  const auth = useAuth();
+  if (auth.isAuth === undefined) {
+    return <Spinner />;
+  }
   return (
     <div>
-      <T4 />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="Home"
+          element={
+            <Wrapper isAuth={auth.isAuth}>
+              <Home />
+            </Wrapper>
+          }
+        />
+        <Route
+          path="About"
+          element={
+            <Wrapper isAuth={auth.isAuth}>
+              <div>About</div>
+            </Wrapper>
+          }
+        />
+      </Routes>
     </div>
   );
 };
